7c568150d04be6970a79b4911603ebbbf7e9e457
fdosreis@fanatics.com <fdosreis@fanatics.com>
2024-03-15 07:30:24 -0400

    Update Files

---
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -0,0 +1,40 @@
+﻿using CleanArchMvc.Domain.Validation;
+using System.Collections.Generic;
+
+namespace CleanArchMvc.Domain.Entities
+{
+    public sealed class Category
+    {
+
+        public Category(string name)
+        {
+            ValidateDomain(name);
+        }
+
+        public Category(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+
+
+        public int Id { get; private set; }
+
+        public string Name { get; private set; }
+
+        public ICollection<Product> Products { get; set; }
+
+
+
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrWhiteSpace(name), "Invalid name, Name is required");
+            DomainExceptionValidation.When(name.Length < 3, "Invalid Name, too Short, minimum 3 characters");
+            Name = name;
+
+        }
+
+
+    }
+}
